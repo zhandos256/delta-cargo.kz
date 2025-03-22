ccb78cd5f1f5e5b07a02dc6787551675d6c99271
zhandos256 <r.zhandos256@gmail.com>
2025-03-22 21:37:15 +0500

    refactor

--- src/config/const.py
+++ src/config/const.py
@@ -13,19 +13,6 @@ TIMEZONE = pytz.timezone("Asia/Almaty")
 
 BOT_TOKEN = "7460694626:AAGyaxk5l3E8Vf8vcMClHYpL2zW6oqgoR7Q"
 
-DB_FILE_PATH = BASE_DIR / 'data.sqlite'
-SCHEDULER_JOBS_FILE_PATH = BASE_DIR / "jobs.sqlite"
+DB_FILE_PATH = BASE_DIR / 'cargo.db'
+SCHEDULER_JOBS_FILE_PATH = BASE_DIR / "jobs.db"
 SCHEDULER_JOBS_DB_URL = f'sqlite:///{SCHEDULER_JOBS_FILE_PATH}'
-
-from aiogram.types import BotCommand
-COMMANDS = [
-    BotCommand(command="/start", description="Template start message"),
-    BotCommand(command="/help", description="Template help message"),
-    BotCommand(command="/menu", description="Template menu message"),
-]
-
-HEADLESS = 1
-
-MAIN_URL_LOGIN = 'https://emir-cargo.kz/login/'
-LOGIN = '87770153025'
-PASSWORD = '123124124g'
