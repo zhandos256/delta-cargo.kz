385105014e0fb41e6d1344c95081a1ca00c9b173
zhandos256 <r.zhandos256@gmail.com>
2025-03-22 20:47:34 +0500

    refactor

--- src/services/parser/utils.py
+++ src/services/parser/utils.py
@@ -4,10 +4,19 @@ import requests
 
 from config.const import BOT_TOKEN
 
-def send_notification(msg: str):
-    chat_id = 6970529864
-    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={msg}'
+
+def send_notification(msg: str, chat_id: int = 6970529864):
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': msg,
+        'parse_mode': 'HTML',  # можно использовать HTML или Markdown для форматирования
+        'disable_web_page_preview': True
+    }
+
     try:
-        resp = requests.post(url=url)
+        response = requests.post(url, data=payload, timeout=5)
+        response.raise_for_status()  # выбросит ошибку, если статус не 200
+        logging.info("📨 Уведомление успешно отправлено")
     except Exception as e:
-        logging.exception(e)
+        logging.exception("❌ Ошибка при отправке уведомления в Telegram")
