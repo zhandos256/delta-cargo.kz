8186946d98ad4868b51f6076f1b8770fde49a006
zhandos256 <r.zhandos256@gmail.com>
2025-03-23 10:05:18 +0500

    refactor

--- src/config/scheduler.py
+++ src/config/scheduler.py
@@ -1,9 +1,16 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
-from config.const import SCHEDULER_JOBS_DB_URL, TIMEZONE
+from config.settings import settings, TIMEZONE
 
 jobstores = {
-    'default': SQLAlchemyJobStore(url=SCHEDULER_JOBS_DB_URL)
+    'default': SQLAlchemyJobStore(url=settings.SCHEDULER_JOBS_DB_URL)
 }
-scheduler = BackgroundScheduler(jobstores=jobstores, timezone=TIMEZONE)
+job_defaults = {
+    'coalesce': True,      # если были пропущенные запуски, выполнить только один
+    'max_instances': 1     # не запускать повторно, если ещё выполняется
+}
+scheduler = BackgroundScheduler(
+    jobstores=jobstores, timezone=TIMEZONE,
+    job_defaults=job_defaults
+)
