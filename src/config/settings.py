f52912cb89c29c1c6753a8b7236eb4812b9418d7
zhandos256 <r.zhandos256@gmail.com>
2025-03-26 13:25:02 +0500

    refactor logger

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import datetime
 
 import pytz
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 TIMEZONE = pytz.timezone("Asia/Almaty")
@@ -11,6 +12,10 @@ LOGS_DIR = BASE_DIR / "logs"
 
 # Создаём директории при необходимости
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def generate_log_file() -> Path:
+    return LOGS_DIR / f"{datetime.now().strftime('%Y-%m-%d')}.log"
 
 class Settings(BaseSettings):
     LOGIN: str
@@ -24,7 +29,7 @@ class Settings(BaseSettings):
     DB_FILE_PATH: Path = BASE_DIR / "cargo.db"
     SCHEDULER_JOBS_FILE_PATH: Path = BASE_DIR / "jobs.db"
     SCHEDULER_JOBS_DB_URL: str = f'sqlite:///{SCHEDULER_JOBS_FILE_PATH}'
-    LOG_FILE: Path = LOGS_DIR / f"{datetime.now().strftime('%Y-%m-%d')}.log"
+    LOG_FILE: Path = Field(default_factory=generate_log_file)
 
     class Config:
         env_file = BASE_DIR / ".env"
